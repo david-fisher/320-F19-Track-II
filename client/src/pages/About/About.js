61d9d96f043b454c6e2dc5e2c9470bcd6a222e99
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2019-12-08 15:18:35 -0500

    Added Picture + formating changes.

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -3,17 +3,33 @@ import "./About.css";
 import { SocialIcon } from "react-social-icons";
 import { Container, Row } from "react-bootstrap";
 
+
 export function About() {
   return (
     <div>
       <Container className="Title">
-        <h1>About us</h1>
+        <h1>About Us</h1>
         <hr />
       </Container>
-
+      <Container>
+          <img src={require('./orchard.jpg')} alt = "Orchard" class = "center"/>
+          <p class = "lead">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pulvinar
+            pharetra felis, id faucibus magna. Donec iaculis sagittis leo, at
+            dictum metus volutpat eget. Praesent iaculis sit amet mauris ut
+            euismod. Aliquam pulvinar consequat facilisis. Cras quis laoreet leo.
+            Cras porttitor iaculis posuere. Maecenas pulvinar, risus non eleifend
+            congue, mauris urna euismod nibh, at venenatis dolor diam id lectus.
+            Ut ut congue mauris. Sed rhoncus commodo velit, a consectetur metus.
+            Morbi justo tortor, aliquet non mi nec, tincidunt condimentum augue.
+            Suspendisse elementum feugiat scelerisque. Integer cursus eros in eros
+            facilisis, id pharetra eros ultrices. Donec aliquet risus eu aliquam
+            elementum.
+          </p>
+      </Container>
       <Container>
         <h2>Follow us on social media:</h2>
-        <Row>
+        <Row className = "SocialIcons">
           <SocialIcon url="http://facebook.com" />
           <SocialIcon url="http://instagram.com" />
           <SocialIcon url="http://twitter.com" />
