0d2148602cf6aefe0ab1e67949011e387ad64414
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2019-12-09 01:30:45 -0500

    Added Gallery Pages

---
+++ client/src/pages/Gallery/Gallery.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Gallery.css";
+import {Container} from "react-bootstrap";
+
+export default function Gallery() {
+  return (
+    <div>
+      <Container className="Title">
+        <h1>Gallery</h1>
+        <hr />
+      </Container>
+    </div>
+  );
+}
