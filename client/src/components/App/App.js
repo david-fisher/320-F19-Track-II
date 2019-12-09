517e2421bc5c21b9a81cb62b181f0550c93f61f5
danzhang41 <33365869+danzhang41@users.noreply.github.com>
2019-12-09 01:12:56 -0500

    Added Gallery

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -22,6 +22,7 @@ export default function App() {
         <Nav className="mr-auto">
           <Nav.Link href="/home">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/gallery">Gallery</Nav.Link>
         </Nav>
 
         <Nav className="justify-content-end">
--- client/src/components/Routes/Routes.js
+++ client/src/components/Routes/Routes.js
@@ -3,6 +3,7 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "../../pages/Home/Home";
 import About from "../../pages/About/About";
 import Dashboard from "../../pages/Dashboard/Dashboard";
+import Gallery from "../../pages/Gallery/Gallery";
 import Login from "../../pages/Login/Login";
 import Profile from "../../pages/Profile/Profile";
 import NotFound from "../../pages/NotFound/NotFound";
@@ -15,6 +16,7 @@ export default function Routes() {
       <Redirect exact from="/" to="/home" />
       <Route path="/home" exact component={Home} />
       <Route path="/about" exact component={About} />
+      <Route path = "/gallery" exact component={Gallery} />>
       <AuthenticatedRoute path="/dashboard" exact component={Dashboard} />
       <UnauthenticatedRoute path="/login" exact component={Login} />
       <AuthenticatedRoute path="/profile" exact component={Profile} />
