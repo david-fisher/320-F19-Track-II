8224fe141c09a3f32e004cce3a39d5eb34c3f309
Zach Schaffer <cursedhydra@gmail.com>
2019-12-18 01:54:28 -0500

    Remove Orchards, Observations, and Dashboard from nav

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -25,14 +25,14 @@ export default function App() {
         <Nav className="mr-auto">
           <Nav.Link href="/home">Home</Nav.Link>
           <Nav.Link href="/about">About Us</Nav.Link>
-          {token !== undefined && (
-            <Nav.Link href="/orchards">Orchards</Nav.Link>
-          )}
+          {/*{token !== undefined && (*/}
+          {/*  <Nav.Link href="/orchards">Orchards</Nav.Link>*/}
+          {/*)}*/}
           {token !== undefined && <Nav.Link href="/data">Data</Nav.Link>}
           {token !== undefined && <Nav.Link href="/gallery">Gallery</Nav.Link>}
-          {token !== undefined && (
-            <Nav.Link href="/observations">Observations</Nav.Link>
-          )}
+          {/*{token !== undefined && (*/}
+          {/*  <Nav.Link href="/observations">Observations</Nav.Link>*/}
+          {/*)}*/}
           {token !== undefined && (
             <Dropdown as={NavItem}>
               <Dropdown.Toggle as={Nav.Link}>Ask AI</Dropdown.Toggle>
@@ -54,7 +54,7 @@ export default function App() {
               <Dropdown.Toggle as={Nav.Link}>{name}</Dropdown.Toggle>
               <Dropdown.Menu alignRight={true}>
                 <Dropdown.Item href="/profile">Profile</Dropdown.Item>
-                <Dropdown.Item href="/dashboard">Dashboard</Dropdown.Item>
+                {/*<Dropdown.Item href="/dashboard">Dashboard</Dropdown.Item>*/}
                 <Dropdown.Item onClick={logout}>Logout</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
