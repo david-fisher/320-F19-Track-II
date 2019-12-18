1791abf8c6d763b2fa7202cb3a1ed456db0f6078
kdstevens <kdstevens@umass.edu>
2019-12-18 01:10:48 -0500

    made a button and css file for predict

--- client/src/pages/Predict/Predict.js
+++ client/src/pages/Predict/Predict.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
+import "./Predict.css";
+
+function openFileExplorer(){
+  document.getElementById("fileInput").click();
+}
 
 export default function Predict() {
   return (
     <Container>
       <h1>Predict</h1>
       <hr />
+      <Button onClick={openFileExplorer}>
+        Predict
+      </Button>
+      <p>
+        Upload an image and click the predict button to see if your apple may have applescab
+      </p>
+      <input className="hiddenFile" id="fileInput" type="file" />
     </Container>
   );
 }
