a4cb80ffcb8245289a9aa7f9a7fbec9ed96b7630
srbalt <44117834+srbalt@users.noreply.github.com>
2019-12-17 19:24:35 -0500

    Throws exception for error handling
    Sorry for ugly commits again, need to do in browser

--- Lambda/src/main/java/entrypoints/HoboPullDataHandler.java
+++ Lambda/src/main/java/entrypoints/HoboPullDataHandler.java
@@ -19,7 +19,7 @@ import java.util.Map;
 public class HoboPullDataHandler extends AbstractHandler {
 
     @Override
-    public GatewayResponse handleRequest(HashMap<String, String> input, Context context)
+    public GatewayResponse handleRequest(HashMap<String, String> input, Context context) throws Exception
     {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
